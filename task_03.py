0688a4b5f1965d7edfdf80e9a90f012439b9a0ee
Niko Vaccaro <mr.nikovaccaro@gmail.com>
2015-09-07 02:55:12 +0000

    changed to charlie to brown in task_03

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Fictional Characters"""
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
